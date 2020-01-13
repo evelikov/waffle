evelikov/waffle

commit e7f0314c7c87e031148d7dee0e6f28d490a97a2a

glx: Don't use ARB_create_context with pre 3.2 contexts
This way if the user requests GL pre 3.2 context which lacks the
flags/extra bits which require ARB_create_context one can safely fall
back to the normal/legacy entry point.

This resolves piglits on non 3.2 capable drivers such as classic swrast,
nouveau_vieux and alike.

v2: The version should be 3.2 and not 3.0 as originally.

Cc: Ilia Mirkin <imirkin@alum.mit.edu>
Reviewed-by: Jose Fonseca <jfonseca@vmware.com> (v1)
Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>
Signed-off-by: Chad Versace <chad.versace@intel.com>

src/waffle/glx/glx_config.c
@@ -32,6 +32,7 @@
 #include "wcore_error.h"
 
 #include "glx_config.h"
+#include "glx_context.h"
 #include "glx_display.h"
 #include "glx_platform.h"
 #include "glx_wrappers.h"
@@ -77,11 +78,11 @@ glx_config_check_context_attrs(struct glx_display *dpy,
 
     switch (attrs->context_api) {
         case WAFFLE_CONTEXT_OPENGL:
-            if (!wcore_config_attrs_version_eq(attrs, 10) && !dpy->ARB_create_context) {
+            if (glx_context_needs_arb_create_context(attrs) &&
+                !dpy->ARB_create_context) {
                 wcore_errorf(WAFFLE_ERROR_UNSUPPORTED_ON_PLATFORM,
                              "GLX_ARB_create_context is required in order to "
-                             "request an OpenGL version not equal to the default "
-                             "value 1.0");
+                             "request an OpenGL version greater or equal than 3.2");
                 return false;
             }
             else if (wcore_config_attrs_version_ge(attrs, 32) && !dpy->ARB_create_context_profile) {

src/waffle/glx/glx_context.c
@@ -173,7 +173,17 @@ glx_context_create_native(struct glx_config *config,
     struct glx_display *dpy = glx_display(config->wcore.display);
     struct glx_platform *platform = glx_platform(dpy->wcore.platform);
 
-    if (dpy->ARB_create_context) {
+    // Use ARB_create_context when we have
+    // - OpenGL version 1.0, or
+    // - OpenGL version 3.2 or greater, or
+    // - OpenGL with fwd_compat, or
+    // - Debug context
+    //
+    // The first one of the four is optional, the remainder hard requirement
+    // for the use of ARB_create_context.
+    if (dpy->ARB_create_context &&
+        (wcore_config_attrs_version_eq(&config->wcore.attrs, 10) ||
+         glx_context_needs_arb_create_context(&config->wcore.attrs))) {
         bool ok;
 
         // Choose a large size to prevent accidental overflow.

src/waffle/glx/glx_context.h
@@ -29,6 +29,7 @@
 
 #include <GL/glx.h>
 
+#include "wcore_config_attrs.h"
 #include "wcore_context.h"
 #include "wcore_util.h"
 
@@ -55,3 +56,18 @@ glx_context_destroy(struct wcore_context *wc_self);
 
 union waffle_native_context*
 glx_context_get_native(struct wcore_context *wc_self);
+
+
+static inline bool
+glx_context_needs_arb_create_context(const struct wcore_config_attrs *attrs)
+{
+    if (attrs->context_api == WAFFLE_CONTEXT_OPENGL &&
+        (wcore_config_attrs_version_ge(attrs, 32) ||
+         attrs->context_forward_compatible))
+        return true;
+
+    if (attrs->context_debug)
+        return true;
+
+    return false;
+}

commit b8a39e6b0a41585d60876854d5b4d8023238a59f

glx: correctly handle ARB_create_context lack/presence
Earlier commit loosened the check of ARB_create_context, for OpenGL 3.2 and
earlier.

Although the commit did not fully consider that the extension is required for
 - all context attributes (missing robustness), and
 - GLES* profiles

As a result, any GL robustness or GLES* context would result in a GL one,
w/o the robustness flag, being created.

Update and sync both attribute validation and runtime check.

Issue: https://github.com/waffle-gl/waffle/issues/50
Fixes: e7f0314c7c8 ("glx: Don't use ARB_create_context with pre 3.2 contexts")
Cc: Jose Fonseca <jfonseca@vmware.com>
Cc: Chad Versace <chad@kiwitree.net>
Signed-off-by: Emil Velikov <emil.velikov@collabora.com>

src/waffle/glx/glx_config.c
@@ -60,30 +60,44 @@ glx_config_check_context_attrs(struct glx_display *dpy,
         assert(wcore_config_attrs_version_ge(attrs, 30));
     }
 
-    if (attrs->context_debug && !dpy->ARB_create_context) {
+    if (!dpy->ARB_create_context &&
+        glx_context_needs_arb_create_context(attrs)) {
+        const char *gl = "";
+        const char *fwd_compat = "";
+        const char *debug = "";
+        const char *robust = "";
+
+        // XXX: Keep in sync with glx_context_needs_arb_create_context()
+        if (attrs->context_api != WAFFLE_CONTEXT_OPENGL)
+	    gl = " - a OpenGL ES* context\n";
+        else if (wcore_config_attrs_version_ge(attrs, 32))
+            gl = " - a OpenGL 3.2+ context\n";
+
+        if (attrs->context_forward_compatible)
+            fwd_compat = " - a forward-compatible context\n";
+
+        if (attrs->context_debug)
+            debug = " - a debug context\n";
+
+        if (attrs->context_robust)
+            robust = " - a robust access context\n";
+
         wcore_errorf(WAFFLE_ERROR_UNSUPPORTED_ON_PLATFORM,
-                     "GLX_ARB_create_context is required in order to "
-                     "request a debug context");
+                     "GLX_ARB_create_context is required to create:\n"
+                     "%s%s%s%s", gl, fwd_compat, debug, robust);
         return false;
     }
 
     if (attrs->context_robust && !dpy->ARB_create_context_robustness) {
         wcore_errorf(WAFFLE_ERROR_UNSUPPORTED_ON_PLATFORM,
-                     "GLX_ARB_create_context_robustness is required in order to "
+                     "GLX_ARB_create_context_robustness is required to "
                      "request a robust access context");
         return false;
     }
 
     switch (attrs->context_api) {
         case WAFFLE_CONTEXT_OPENGL:
-            if (glx_context_needs_arb_create_context(attrs) &&
-                !dpy->ARB_create_context) {
-                wcore_errorf(WAFFLE_ERROR_UNSUPPORTED_ON_PLATFORM,
-                             "GLX_ARB_create_context is required in order to "
-                             "request an OpenGL version greater or equal than 3.2");
-                return false;
-            }
-            else if (wcore_config_attrs_version_ge(attrs, 32) && !dpy->ARB_create_context_profile) {
+            if (wcore_config_attrs_version_ge(attrs, 32) && !dpy->ARB_create_context_profile) {
                 wcore_errorf(WAFFLE_ERROR_UNSUPPORTED_ON_PLATFORM,
                              "GLX_ARB_create_context_profile is required "
                              "to create a context with version >= 3.2");
@@ -92,13 +106,6 @@ glx_config_check_context_attrs(struct glx_display *dpy,
             else if (wcore_config_attrs_version_ge(attrs, 32)) {
                 assert(attrs->context_profile == WAFFLE_CONTEXT_CORE_PROFILE ||
                        attrs->context_profile == WAFFLE_CONTEXT_COMPATIBILITY_PROFILE);
-            }
-
-            if (attrs->context_forward_compatible && !dpy->ARB_create_context) {
-                wcore_errorf(WAFFLE_ERROR_UNSUPPORTED_ON_PLATFORM,
-                             "GLX_ARB_create_context is required in order to "
-                             "request a forward-compatible context");
-                return false;
             }
 
             return true;

src/waffle/glx/glx_context.c
@@ -170,20 +170,15 @@ glx_context_create_native(struct glx_config *config,
 {
     GLXContext ctx;
     GLXContext real_share_ctx = share_ctx ? share_ctx->glx : NULL;
+    const struct wcore_config_attrs *attrs = &config->wcore.attrs;
     struct glx_display *dpy = glx_display(config->wcore.display);
     struct glx_platform *platform = glx_platform(dpy->wcore.platform);
 
-    // Use ARB_create_context when we have
-    // - OpenGL version 1.0, or
-    // - OpenGL version 3.2 or greater, or
-    // - OpenGL with fwd_compat, or
-    // - Debug context
-    //
-    // The first one of the four is optional, the remainder hard requirement
-    // for the use of ARB_create_context.
+    // Optionally use ARB_create_context for OpenGL 1.0, if available.
     if (dpy->ARB_create_context &&
-        (wcore_config_attrs_version_eq(&config->wcore.attrs, 10) ||
-         glx_context_needs_arb_create_context(&config->wcore.attrs))) {
+        (glx_context_needs_arb_create_context(attrs) ||
+         (attrs->context_api == WAFFLE_CONTEXT_OPENGL &&
+          wcore_config_attrs_version_eq(attrs, 10)))) {
         bool ok;
 
         // Choose a large size to prevent accidental overflow.

src/waffle/glx/glx_context.h
@@ -58,15 +58,29 @@ union waffle_native_context*
 glx_context_get_native(struct wcore_context *wc_self);
 
 
+// XXX: Keep in sync with glx_config_check_context_attrs()
 static inline bool
 glx_context_needs_arb_create_context(const struct wcore_config_attrs *attrs)
 {
-    if (attrs->context_api == WAFFLE_CONTEXT_OPENGL &&
-        (wcore_config_attrs_version_ge(attrs, 32) ||
-         attrs->context_forward_compatible))
+    // Any of the following require the ARB extension, since the data is
+    // passed as attributes.
+
+    // Using any GLES* profile,
+    if (attrs->context_api != WAFFLE_CONTEXT_OPENGL)
+        return true;
+
+    // explicitly requesting OpenGL version (>=3.2),
+    if (wcore_config_attrs_version_ge(attrs, 32))
+        return true;
+
+    // ... or any of the context flags.
+    if (attrs->context_forward_compatible)
         return true;
 
     if (attrs->context_debug)
+        return true;
+
+    if (attrs->context_robust)
         return true;
 
     return false;